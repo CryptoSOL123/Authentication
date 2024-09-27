7860ef4e282334f14124d9f87dd889e37c8fd635
CryptoSOL123 <163804105+CryptoSOL123@users.noreply.github.com>
2024-09-27 07:15:27 -0700

    Updated login and signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Route, Routes } from "react-router-dom";
-import "./components/Menubar";
-import "./components/Login";
-import "./components/Signup";
 import Menubar from "./components/Menubar";
 import Login from "./components/Login";
 import Register from "./components/Signup";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Menubar />} />
-      <Route path="log" element={<Login />} />
-      <Route path="register" element={<Register />} />
-    </Routes>
+    <>
+      {/* Render Menubar outside of Routes so it stays on all pages */}
+      <Menubar />
+      <Routes>
+        <Route path="/" element={<div>Home Page Content</div>} />
+        <Route path="log" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
